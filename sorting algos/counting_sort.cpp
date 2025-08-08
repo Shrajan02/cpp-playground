0f30f9cbf20f5ae07e3c3feb673ff6701c811ef5
Shrajan Karkera <shrajankarkera02@gmail.com>
2025-08-08 15:19:28 +0530

    Commit C1 - initial file setup

---
+++ sorting algos/counting_sort.cpp
@@ -0,0 +1,53 @@
+// TC: O(N + maxi)
+// SC: O(N + maxi)
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> countingSort(vector<int>& arr, int n) {
+    // create a frequency array of length max_element(arr) + 1
+    // where every index corresponds to unique element within that range
+    // convert it into a cumulative frequency array
+    // using both these arrays, do a backward traversal over original array
+    int maxi = *max_element(arr.begin(), arr.end());
+    vector<int> frequency(maxi + 1, 0);
+    vector<int> res(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        frequency[arr[i]]++;
+    }
+
+    for (int i = 0; i < maxi; i++)
+    {
+        frequency[i + 1] += frequency[i];
+    }
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        int index = --frequency[arr[i]];
+        res[index] = arr[i];
+    }
+
+    return res;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    vector<int> arr;
+    int val;
+    while (cin >> val) {
+        arr.push_back(val);
+    }
+
+    int n = arr.size();
+    vector<int> res = countingSort(arr, n);
+    for (int num : res)
+    {
+        cout << num << " ";
+    }
+
+    return 0;
+}
