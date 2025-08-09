22fefa3348c03761e79073693d2b0c31adf2ac36
Shrajan Karkera <shrajankarkera02@gmail.com>
2025-08-09 13:11:01 +0530

    Commit C4 - n^2 algos completed

---
+++ sorting algos/bubble_sort.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(vector<int>& arr, int n) {
+    // bubbling up the largest element
+    // i -> pass counter
+    // j -> adjacent pair check
+    for (int i = 0; i < n - 1; i++) {
+        bool swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped) {
+            break;  
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    vector<int> arr;
+    int val;
+    while (cin >> val) {
+        arr.push_back(val);
+    }
+    int n = arr.size();
+
+    bubbleSort(arr, n);
+    for (int num: arr) {
+        cout << num << " ";
+    }
+
+    return 0;
+}
---
+++ sorting algos/selection_sort.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void selectionSort(vector<int>& arr, int n) {
+    // selecting index of the minimum 
+    // i -> sorted portion (position where next smallest element should go)
+    // j -> unsorted portion
+    for (int i = 0; i < n - 1; i++) {
+        int min_index = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_index]) {
+                min_index = j;
+            }
+        }
+        if (min_index != i) {
+            std::swap(arr[min_index], arr[i]);
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    vector<int> arr;
+    int val;
+    while (cin >> val) {
+        arr.push_back(val);
+    }
+    int n = arr.size();
+
+    selectionSort(arr, n);
+    for (int num: arr) {
+        cout << num << " ";
+    }
+
+    return 0;
+}
