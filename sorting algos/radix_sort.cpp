e2ea8c46ed1247d23bb54d6c274ba3fbf2dddacb
Shrajan Karkera <shrajankarkera02@gmail.com>
2025-08-11 16:02:23 +0530

    Commit C6 - radix sort completed

--- sorting algos/radix_sort.cpp
+++ sorting algos/radix_sort.cpp
@@ -1,8 +1,37 @@
+// TC: O(d * (N + b)) 
+// SC: O(N + b)
+// where, d -> number of digits, b -> range (10 for decimal)
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> radixSort(vector<int>& arr, int n) {
-    
+vector<int> countSort(const vector<int>& arr, int n, int exp) {
+    vector<int> res(n);  // N
+    vector<int> frequency(10, 0);  // b
+
+    for (int i = 0; i < n; i++) {
+        int digit = (arr[i] / exp) % 10;
+        frequency[digit]++;
+    }
+    for (int i = 1; i < 10; i++) {
+        frequency[i] += frequency[i - 1];
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        int digit = (arr[i] / exp) % 10;
+        int index = --frequency[digit];
+        res[index] = arr[i];
+    }
+
+    return res;
+}
+
+void radixSort(vector<int>& arr, int n) {
+    int maxi = *max_element(arr.begin(), arr.end());
+
+    // apply counting sort on every digit starting from LSD
+    for (int exponent = 1; maxi / exponent > 0; exponent *= 10) {
+        arr = countSort(arr, n, exponent);  // 1, 10, 100 ,..
+    }
 }
 
 int main() {
@@ -16,8 +45,8 @@ int main() {
     }
     int n = arr.size();
 
-    vector<int> res = radixSort(arr, n);
-    for (int num: res) {
+    radixSort(arr, n);
+    for (int num : arr) {
         cout << num << " ";
     }
 
