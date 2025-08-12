d4408e448293730d20e7517ec44a38543465efb9
Shrajan Karkera <shrajankarkera02@gmail.com>
2025-08-13 02:14:44 +0530

    Commit C9 - added bucket sort

--- sorting algos/bucket_sort.cpp
+++ sorting algos/bucket_sort.cpp
@@ -1,8 +1,30 @@
+// TC: O(N*log(N))
+// SC: O(N)
 #include <bits/stdc++.h>
 using namespace std;
 
 vector<int> bucketSort(vector<int>& arr, int n) {
-    
+    // insert arr[i] into buckets[n*arr[i]]
+    int maxi = *max_element(arr.begin(), arr.end());
+    vector<vector<int>> buckets(n);
+    for (int num: arr) {
+        int index = (1LL * n * num) / (maxi + 1); 
+        buckets[index].push_back(num);
+    }
+
+    // sort buckets using comparative algo
+    for (auto& bucket: buckets) {
+        std::sort(bucket.begin(), bucket.end());
+    }
+
+    vector<int> result;
+    for (auto& bucket: buckets) {
+        for (int val: bucket) {
+            result.push_back(val);
+        }
+    }
+
+    return result;
 }
 
 int main() {
